0c26ac81e70ccf4ab8d3a4569d7867996c6f61bd
Gustavo de Vito <gustavodevito38@gmail.com>
2024-03-13 07:16:16 -0300

    feat: flow validation env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { TodosModule } from './todos/todos.module';
+import { ConfigModule } from '@nestjs/config';
+import { validate } from './validations/env.validation';
 
 @Module({
-  imports: [PrismaModule, TodosModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, validate }),
+    PrismaModule,
+    TodosModule,
+  ],
 })
 export class AppModule {}
